2dd6721f64afe26b2356ce0fd679470fc965462b
jddgt <copskull329@gmail.com>
2025-10-05 12:40:55 -0400

    2 - Major Changes
    Moved option int(input()), compiled the print() statements into one with \newline and added while loops to get some things to repeat.

--- 2 - What Option?.py
+++ 2 - What Option?.py
@@ -1,21 +1,30 @@
-option = int(input("Input a number. What option would you like to choose?: "))
-print("1 - Chicken")
-print("2 - Beef")
-print("3 - Turkey")
-print("4 - Fish")
-print()
-match option:
-
-    case 1:
-        print("Good choice, chicken is a to-go to for a lean meat")
-    case 2:
-        print("Beef is not bad, but it can be fatty.")
-    case 3:
-        print("Turkey is like beef but with less of the fat, so would it be better?")
-    case 4:
-        print("Maybe the weakest link among the others, but it's still a solid protein.")
-    case _:
-        print("If you're reading this, you didn't put a number 1-4, shame on you.")
-
-if option == 1 or option == 2 or option == 3 or option == 4:
-    print("Bur thanks for sharing, bye!")
+while True:
+    print(("Input a number. What option would you like to choose?: "
+           "\n1 - Chicken"
+           "\n2 - Beef"
+           "\n3 - Turkey"
+           "\n4 - Fish"))
+    option = int(input())
+    print()
+    while True:
+        match option:
+            case 1:
+                print("Good choice, chicken is a to-go to for a lean meat")
+                break
+            case 2:
+                print("Beef is not bad, but it can be fatty.")
+                break
+            case 3:
+                print("Turkey is like beef but with less of the fat, so would it be better?")
+                break
+            case 4:
+                print("Maybe the weakest link among the others, but it's still a solid protein.")
+            case _:
+                print("If you're reading this, you didn't put a number 1-4, shame on you.")
+    print()
+    again = input("Want to select something else? ")
+    if again in ("Yes", "yes", "YES" "Y", "y"):
+        print()
+        continue
+    else:
+        print("Bur thanks for sharing. Buh-bye")
