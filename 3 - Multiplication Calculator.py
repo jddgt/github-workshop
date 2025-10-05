c9d41bfb93ed995d9a8da0800289ada75506de20
jddgt <copskull329@gmail.com>
2025-10-05 12:50:20 -0400

    3 - Title and if Statement
    Added an if statement for when the user has an operand equal to zero.

--- 3 - Multiplication Calculator.py
+++ 3 - Multiplication Calculator.py
@@ -1,12 +1,16 @@
 product = 0
 
-print("Here's a multiplication calculator:")
+print("[Multiplication Calculator]")
 print()
 while True:
     operand_1 = int(input("Enter an operand: "))
     operand_2 = int(input("Enter another operand: "))
-    for o_repeats in range(0, operand_2):
-        product += operand_1
-    print(f"{operand_1} x {operand_2} = {product}")
-    print()
-    product = 0
+    if operand_1 == 0 or operand_2 == 0:
+        print("Why should you calculate this? The answer is always zero!")
+        print()
+    else:
+        for o_repeats in range(0, operand_2):
+            product += operand_1
+        print(f"{operand_1} x {operand_2} = {product}")
+        print()
+        product = 0
